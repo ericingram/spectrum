7382703cf83db80b6ec71c42fc49ccfb624bf54b
mkharitonov <m.v.kharitonov@gmail.com>
2011-12-04 20:03:23 +0300

    Remove "PHPUnit/Framework.php" include

--- tests/init.php
+++ tests/init.php
@@ -15,5 +15,5 @@
  */
 require_once dirname(__FILE__) . '/autoload.php';
 require_once dirname(__FILE__) . '/../application/autoload.php';
-require_once 'PHPUnit/Framework.php';
+
 set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/../application/matchers');
