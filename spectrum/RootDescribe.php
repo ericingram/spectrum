fc19fb990b6a27e175bfada839f49ec6658bb0cd
mkharitonov <m.v.kharitonov@gmail.com>
2011-12-11 21:41:51 +0300

    Refactor "RootDescribe" class

--- spectrum/RootDescribe.php
+++ spectrum/RootDescribe.php
@@ -36,21 +36,29 @@ class RootDescribe
 	{
 		if (!static::$onceInstance)
 		{
-			$describeClass = \net\mkharitonov\spectrum\core\Config::getSpecContainerDescribeClass();
-			static::$onceInstance = new $describeClass();
-
-			static::$onceInstance->errorHandling->setCatchExceptions(true);
-			static::$onceInstance->errorHandling->setCatchPhpErrors(true);
-			static::$onceInstance->errorHandling->setBreakOnFirstPhpError(false);
-			static::$onceInstance->errorHandling->setBreakOnFirstMatcherFail(false);
-
-			static::addAllMatchersTo(static::$onceInstance);
+			static::$onceInstance = static::createSpecContainer();
+			static::configureSpec(static::$onceInstance);
+			static::addMatchersToSpec(static::$onceInstance);
 		}
 
 		return static::$onceInstance;
 	}
 
-	static public function addAllMatchersTo(\net\mkharitonov\spectrum\core\SpecInterface $spec)
+	static protected function createSpecContainer()
+	{
+		$describeClass = \net\mkharitonov\spectrum\core\Config::getSpecContainerDescribeClass();
+		return new $describeClass();
+	}
+
+	static protected function configureSpec(\net\mkharitonov\spectrum\core\SpecContainerInterface $spec)
+	{
+		$spec->errorHandling->setCatchExceptions(true);
+		$spec->errorHandling->setCatchPhpErrors(true);
+		$spec->errorHandling->setBreakOnFirstPhpError(false);
+		$spec->errorHandling->setBreakOnFirstMatcherFail(false);
+	}
+
+	static protected function addMatchersToSpec(\net\mkharitonov\spectrum\core\SpecContainerInterface $spec)
 	{
 		$spec->matchers->add('null', '\net\mkharitonov\spectrum\matchers\null');
 		$spec->matchers->add('true', '\net\mkharitonov\spectrum\matchers\true');
