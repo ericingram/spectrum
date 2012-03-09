472afb455adb7cd073d3a2db3da0eac9ad2b36f9
Mikhail Kharitonov <m.v.kharitonov@gmail.com>
2012-03-09 19:22:45 +0400

    "Report" plugin changes: added closure code displaying

--- spectrum/core/plugins/basePlugins/report/components/code/variables/ClosureVar.php
+++ spectrum/core/plugins/basePlugins/report/components/code/variables/ClosureVar.php
@@ -19,8 +19,41 @@ class ClosureVar extends Variable
 {
 	protected $type = 'closure';
 
+	public function getStyles()
+	{
+		$componentSelector = '.g-code-variables-' . htmlspecialchars($this->type);
+
+		return
+			parent::getStyles() . $this->getNewline() .
+			'<style type="text/css">' . $this->getNewline() .
+				$this->getIndention() . "$this->expandedParentSelector $componentSelector .value { white-space: pre; }" . $this->getNewline() .
+			'</style>' . $this->getNewline();
+	}
+
 	protected function getHtmlForValue($variable)
 	{
-		return '';
+		return ' <span class="value">' . htmlspecialchars($this->getClosureCode($variable)) . '</span>';
+	}
+
+	protected function getClosureCode($variable)
+	{
+		$reflection = new \ReflectionFunction($variable);
+		$args = array();
+		foreach ($reflection->getParameters() as $param)
+			$args[] = '$' . $param->getName();
+
+		$file = new \SplFileObject($reflection->getFileName());
+		$file->seek($reflection->getStartLine() - 1);
+		$code = '';
+		while ($file->key() < $reflection->getEndLine())
+		{
+			$code .= $file->current();
+			$file->next();
+		}
+
+		$start = mb_strpos($code, '{');
+		$end = mb_strrpos($code, '}');
+
+		return 'function(' . implode(', ', $args) . ') ' . mb_substr($code, $start, $end - $start);
 	}
 }
