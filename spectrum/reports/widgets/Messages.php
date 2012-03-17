60ef308f8d5ce120ac45bacf5496f1d0b80a6253
Mikhail Kharitonov <m.v.kharitonov@gmail.com>
2012-03-17 23:40:01 +0400

    reports\widgets\Messages: added order number to message list

--- spectrum/reports/widgets/Messages.php
+++ spectrum/reports/widgets/Messages.php
@@ -25,6 +25,7 @@ class Messages extends \net\mkharitonov\spectrum\reports\widgets\Widget
 				$this->getIndention() . '.g-messages>h1 { float: left; margin-bottom: 2px; padding: 0.3em 0.5em 0 0.5em; color: #888; font-size: 0.9em; font-weight: normal; }' . $this->getNewline() .
 				$this->getIndention() . '.g-messages>ul { clear: both; float: left; list-style: none; }' . $this->getNewline() .
 				$this->getIndention() . '.g-messages>ul>li { margin-bottom: 2px; padding: 0.4em 0.5em; border-radius: 4px; background: #ddd; }' . $this->getNewline() .
+				$this->getIndention() . '.g-messages>ul>li>.number { color: #888; font-size: 0.9em; }' . $this->getNewline() .
 			'</style>' . $this->getNewline();
 	}
 
@@ -40,12 +41,21 @@ class Messages extends \net\mkharitonov\spectrum\reports\widgets\Widget
 		$output .= '<div class="g-messages g-clearfix">' . $this->getNewline();
 		$output .= $this->getIndention() . '<h1>' . $this->translate('Messages') . ':</h1>' . $this->getNewline();
 		$output .= $this->getIndention() . '<ul>' . $this->getNewline();
+		$num = 0;
 		foreach ($messages as $message)
-			$output .= $this->getIndention(2) . '<li>' . htmlspecialchars($message) . '</li>' . $this->getNewline();
+		{
+			$num++;
+			$output .= $this->getIndention(2) . '<li>' . $this->getHtmlForNumber($num) . htmlspecialchars($message) . '</li>' . $this->getNewline();
+		}
 
 		$output .= $this->getIndention() . '</ul>' . $this->getNewline();
 		$output .= '</div>' . $this->getNewline();
 
 		return $output;
+	}
+
+	protected function getHtmlForNumber($num)
+	{
+		return '<span class="number">' . htmlspecialchars($num) . '. </span>';
 	}
 }
