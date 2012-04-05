6bb12e5b079dad5bd8b44afc175f1b7470329530
Mikhail Kharitonov <m.v.kharitonov@gmail.com>
2012-04-05 15:42:07 +0400

    Corrected "LICENSE.txt" and license header in files

--- tests/core/specItemIt/Test.php
+++ tests/core/specItemIt/Test.php
@@ -1,12 +1,9 @@
 <?php
 /*
- * Spectrum
+ * (c) Mikhail Kharitonov <mvkharitonov@gmail.com>
  *
- * Copyright (c) 2011 Mikhail Kharitonov <mvkharitonov@gmail.com>
- * All rights reserved.
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
+ * For the full copyright and license information, see the
+ * LICENSE.txt file that was distributed with this source code.
  */
 
 namespace spectrum\core\specItemIt;
