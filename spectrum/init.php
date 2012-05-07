8b452da1e318da21f946d20f64dd2066931ef029
Mikhail Kharitonov <m.v.kharitonov@gmail.com>
2012-05-07 16:41:11 +0400

    globalAliases.php moved to init.php

--- spectrum/init.php
+++ spectrum/init.php
@@ -6,6 +6,46 @@
  * LICENSE.txt file that was distributed with this source code.
  */
 
-require_once dirname(__FILE__) . '/autoload.php';
-\spectrum\core\plugins\Manager::registerPlugin('reports', '\spectrum\reports\Plugin', 'whenFirstAccess');
-require_once dirname(__FILE__) . '/constructionCommands/globalAliases.php';
+require_once __DIR__ . '/autoload.php';
+\spectrum\core\plugins\Manager::registerPlugin('reports', '\spectrum\reports\Plugin');
+
+
+/* Functions declaring for IDE autocompletion support */
+
+/**
+ * @return \spectrum\core\SpecContainerDescribe
+ */
+function describe()      { return call_user_func_array('\spectrum\constructionCommands\Manager::' . __FUNCTION__, func_get_args()); }
+
+/**
+ * @return \spectrum\core\SpecContainerContext
+ */
+function context()       { return call_user_func_array('\spectrum\constructionCommands\Manager::' . __FUNCTION__, func_get_args()); }
+
+/**
+ * @return \spectrum\core\SpecItemIt
+ */
+function it()            { return call_user_func_array('\spectrum\constructionCommands\Manager::' . __FUNCTION__, func_get_args()); }
+
+/**
+ * @return \spectrum\core\SpecContainerPattern
+ */
+function itLikePattern() { return call_user_func_array('\spectrum\constructionCommands\Manager::' . __FUNCTION__, func_get_args()); }
+function addPattern()    { return call_user_func_array('\spectrum\constructionCommands\Manager::' . __FUNCTION__, func_get_args()); }
+
+function addMatcher()    { return call_user_func_array('\spectrum\constructionCommands\Manager::' . __FUNCTION__, func_get_args()); }
+
+function beforeEach()    { return call_user_func_array('\spectrum\constructionCommands\Manager::' . __FUNCTION__, func_get_args()); }
+function afterEach()     { return call_user_func_array('\spectrum\constructionCommands\Manager::' . __FUNCTION__, func_get_args()); }
+
+/**
+ * @return \spectrum\core\asserts\Assert
+ */
+function the()           { return call_user_func_array('\spectrum\constructionCommands\Manager::' . __FUNCTION__, func_get_args()); }
+
+/**
+ * @return \spectrum\core\WorldInterface
+ */
+function world()           { return call_user_func_array('\spectrum\constructionCommands\Manager::' . __FUNCTION__, func_get_args()); }
+function fail()          { return call_user_func_array('\spectrum\constructionCommands\Manager::' . __FUNCTION__, func_get_args()); }
+function message()       { return call_user_func_array('\spectrum\constructionCommands\Manager::' . __FUNCTION__, func_get_args()); }
